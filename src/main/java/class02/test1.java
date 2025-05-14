b36ac0dc2ce12e928462ea862203b8e8dae9704e
Papdianasz <papdianasz@gmail.com>
2025-05-14 19:58:08 -0400

    second commit

--- src/main/java/class02/test1.java
+++ src/main/java/class02/test1.java
@@ -4,5 +4,14 @@ public class test1 {
     public static void main(String[] args) {
 
         System.out.println("hello");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
